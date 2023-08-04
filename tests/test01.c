05c2b1289636634978c570b26284cd31eabb7f8a
A-Koderr <Ars_414b@protonmail.com>
2023-08-04 23:35:29 +0530

    Rewrote test01

--- tests/test01.c
+++ tests/test01.c
@@ -2,13 +2,20 @@
 #include <stdio.h>
 #include "stack/stack.h"
 
+#define el 5
+
 int main(void) {
     stack *ST = stack_init(LONG);
     long kk = 78;
-    for(int i=0; i<5; ++i) {
+    for(int i=0; i<el; ++i) {
         ++kk;
         stack_push(ST, &kk);
     }
-    stack_print(ST);
+    for(int i=0; i<el+1; ++i) {
+        printf("Current Stack : \n");
+        stack_print(ST);
+        printf("\n");
+        stack_pop(ST);
+    }
     stack_free(ST);
 }
